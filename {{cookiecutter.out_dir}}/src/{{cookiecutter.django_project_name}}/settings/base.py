29575c1810267822aa95c88498073910c6a3db15
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-01-08 10:54:34 +0100

    style

--- {{cookiecutter.out_dir}}/src/{{cookiecutter.django_project_name}}/settings/base.py
+++ {{cookiecutter.out_dir}}/src/{{cookiecutter.django_project_name}}/settings/base.py
@@ -1,15 +1,4 @@
-from __future__ import absolute_import, division, print_function
-
-import copy
-import os
-from datetime import timedelta
-from importlib import import_module
-
-import django
-import six
-from django.utils.log import DEFAULT_LOGGING
-
-
+# -*- coding: utf-8 -*-
 """
 Django settings for {{cookiecutter.django_project_name}} project.
 
@@ -21,67 +10,16 @@ https://docs.djangoproject.com/en/{{cookiecutter.django_ver_1}}/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/{{cookiecutter.django_ver_1}}/ref/settings/
 """
+from __future__ import absolute_import, division, print_function
 
+import copy
+import os
+from datetime import timedelta
+from importlib import import_module
 
-def as_col(value, separators=None, final_type=None, **kw):
-    if final_type is None:
-        final_type = list
-    if separators is None:
-        separators = ['-|_', '_|-', '___', ',', ';', '|']
-    if isinstance(value, six.string_types):
-        assert(len(separators))
-        while separators:
-            try:
-                separator = separators.pop(0)
-            except IndexError:
-                break
-            if separator in value:
-                break
-        value = final_type(value.split(separator))
-        if final_type is not list:
-            value = final_type(value)
-    return value
-
-
-def as_int(value, **kw):
-    if value not in ['', None]:
-        value = int(value)
-    return value
-
-
-def as_bool(value, asbool=True):
-    if isinstance(value, six.string_types):
-        if value and asbool:
-            low = value.lower().strip()
-            if low in [
-                'false', 'non', 'no', 'n', 'off', '0', '',
-            ]:
-                return False
-            if low in [
-                'true', 'oui', 'yes', 'y', 'on', '1',
-            ]:
-                return True
-    return bool(value)
-
-
-def locals_settings_update(locs_, d=None):
-    if d is None:
-        d = {}
-    for a, b in six.iteritems(d):
-        if a in [
-            '__name__', '__doc__', '__package__',
-            '__loader__', '__spec__', '__file__',
-            '__cached__', '__builtins__'
-        ]:
-            continue
-        locs_[a] = b
-    return locs_, d.get('__name__', '').split('.')[-1]
-
-
-def filter_globals(locs_, d=None):
-    if d is None:
-        d = {}
-    return locals_settings_update({}, globals())[0]
+import django
+import six
+from django.utils.log import DEFAULT_LOGGING
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -191,9 +129,9 @@ db_opts = (
 )
 db_opts_dict = dict(db_opts)
 db_opts_k = 'general'
-for k, values in db_opts:
-    if k in DEFAULT_DB['ENGINE']:
-        db_opts_k = k
+for knob, values in db_opts:
+    if knob in DEFAULT_DB['ENGINE']:
+        db_opts_k = knob
         break
 for v, envvars in db_opts_dict[db_opts_k].items():
     for envvar in envvars + db_opts_dict['general'][v]:
@@ -272,6 +210,17 @@ CORS_ORIGIN_ALLOW_ALL = True
 EMAIL_HOST = 'mailcatcher'
 EMAIL_PORT = 1025
 
+###############################################################################
+# Environment settings routines, compliant with 12Factor: https://12factor.net/
+#  The settings are loaded (first has more priority):
+#  - maybe from: .instances.<env>
+#  - maybe from: .local
+#  - one of: .{prod, dev, test}
+#  - environ: Most of the base variables can be redefined
+#    via environment variables
+#  - this file: .base
+###############################################################################
+
 # Make django configurable via environment
 SETTINGS_ENV_PREFIX = 'DJANGO__'
 # Those settings will throw a launch failure in deploy envs
@@ -287,41 +236,114 @@ ENV_VARS = EXPLICIT_ENV_VARS + [
 DJANGO_ENV_VARS = {}
 
 # Scan environ for django configuration items
-for i, val in os.environ.items():
+for cenvvar, value in os.environ.items():
     # Bring back prefixed env vars
     # eg DJANGO__SECRET_KEY to SECRET_KEY form.
-    if i.startswith(SETTINGS_ENV_PREFIX):
-        s = SETTINGS_ENV_PREFIX.join(i.split(SETTINGS_ENV_PREFIX)[1:])
-        DJANGO_ENV_VARS[s] = val
+    if var.startswith(SETTINGS_ENV_PREFIX):
+        setting = SETTINGS_ENV_PREFIX.join(
+            cenvvar.split(SETTINGS_ENV_PREFIX)[1:])
+        DJANGO_ENV_VARS[setting] = value
     #
     # Look also at the environ Root for explicit env vars
     #  Please note that prefixed value will always have
     #  the higher priority (DJANGO__FOO vs FOO)
-    for s in ENV_VARS:
-        if s not in DJANGO_ENV_VARS:
+    for setting in ENV_VARS:
+        if setting not in DJANGO_ENV_VARS:
             try:
-                DJANGO_ENV_VARS[s] = os.environ[s]
+                DJANGO_ENV_VARS[setting] = os.environ[setting]
             except KeyError:
                 pass
 
 # export back DJANGO_ENV_VARS dict as django settings
 globs = globals()
-for s, val in six.iteritems(DJANGO_ENV_VARS):
-    globs[s] = val
+for setting, value in six.iteritems(DJANGO_ENV_VARS):
+    globs[setting] = value
+
+
+def as_col(value, separators=None, final_type=None, **kw):
+
+    if final_type is None:
+        final_type = list
+    if separators is None:
+        separators = ['-|_', '_|-', '___', ',', ';', '|']
+    if isinstance(value, six.string_types):
+        assert(len(separators))
+        while separators:
+            try:
+                separator = separators.pop(0)
+            except IndexError:
+                break
+            if separator in value:
+                break
+        value = final_type(value.split(separator))
+        if final_type is not list:
+            value = final_type(value)
+    return value
+
+
+def as_int(value, **kw):
+    if value not in ['', None]:
+        value = int(value)
+    return value
+
+
+def as_bool(value, asbool=True):
+    if isinstance(value, six.string_types):
+        if value and asbool:
+            low = value.lower().strip()
+            if low in [
+                'false', 'non', 'no', 'n', 'off', '0', '',
+            ]:
+                return False
+            if low in [
+                'true', 'oui', 'yes', 'y', 'on', '1',
+            ]:
+                return True
+    return bool(value)
+
+
+def locals_settings_update(_locals, mapping=None):
+    '''
+    Update _locals dict with mapping dict filtering module special variables.
+    mapping: any dict, and potentially a globals/locals __dict__
+
+    return:
+        _locals: updated with mapping vars/vals
+        __name__ if any: most of the cases, it's use to determine the environment (name)
+    '''
+    if mapping is None:
+        mapping = {}
+    for var, value in six.iteritems(mapping):
+        if var in [
+            '__name__', '__doc__', '__package__',
+            '__loader__', '__spec__', '__file__',
+            '__cached__', '__builtins__'
+        ]:
+            continue
+        _locals[var] = value
+    return _locals, mapping.get('__name__', '').split('.')[-1]
+
+
+def filter_globals(_locals, mapping=None):
+    '''
+    Shortcut help to only get locals_ from the locals_settings_update call;
+    with current module globals
+    '''
+    if mapping is None:
+        mapping = globals()
+    return locals_settings_update({}, mapping)[0]
+
 
 
 def check_explicit_settings(globs=None):
     '''
     verify that some vars are explicitly defined
     '''
-    locs_, env = locals_settings_update(locals(), globs)
-    for i in EXPLICIT_ENV_VARS:
+    for setting in EXPLICIT_ENV_VARS:
         try:
-            _ = locs_[i]  #noqa
+            _ = _locals[setting]  #noqa
         except KeyError:
             raise Exception('{0} django settings is not defined')
-    globals().update(locs_)
-    return locs_, filter_globals(globals()), env
 
 
 def post_process_settings(globs=None):
@@ -331,8 +353,9 @@ def post_process_settings(globs=None):
         - tranforming vars which can come from system environment as strings
           in their final values as django settings
     '''
-    locs_, globs, env = check_explicit_settings(globs)
-    for setting, func, fkw in (
+    _locals, env = locals_settings_update(locals(), globs)
+    check_explicit_settings(_locals)
+    for setting, func, fkwargs in (
         ('EMAIL_PORT', as_int, {}),
         ('EMAIL_USE_TLS', as_bool, {}),
         ('CORS_ORIGIN_ALLOW_ALL', as_bool, {}),
@@ -340,22 +363,23 @@ def post_process_settings(globs=None):
         ('ALLOWED_HOSTS', as_col, {}),
     ):
         try:
-            locs_[setting]
+            _locals[setting]
         except KeyError:
             continue
-        locs_[setting] = func(locs_[setting], **fkw)
-    {% if cookiecutter.with_sentry -%}SENTRY_DSN = locs_.setdefault('SENTRY_DSN', '')
-    SENTRY_RELEASE = locs_.setdefault('SENTRY_RELEASE', 'prod')
-    INSTALLED_APPS = locs_.setdefault('INSTALLED_APPS', tuple())
-    SENTRY_TAGS = locs_.pop('SENTRY_TAGS', None)
+        _locals[setting] = func(_locals[setting], **fkwargs)
+    {% if cookiecutter.with_sentry -%}SENTRY_DSN = _locals.setdefault('SENTRY_DSN', '')
+    SENTRY_RELEASE = _locals.setdefault('SENTRY_RELEASE', 'prod')
+    INSTALLED_APPS = _locals.setdefault('INSTALLED_APPS', tuple())
+    SENTRY_TAGS = _locals.pop('SENTRY_TAGS', None)
     if SENTRY_DSN:
         if 'raven.contrib.django.raven_compat' not in INSTALLED_APPS:
-            locs_['INSTALLED_APPS'] = (
+            # type is used to handle both INSTALLED_APPS setted as a tuple or a list
+            _locals['INSTALLED_APPS'] = (
                 type(
-                    locs_['INSTALLED_APPS']
+                    _locals['INSTALLED_APPS']
                 )(['raven.contrib.django.raven_compat']) +
-                locs_['INSTALLED_APPS'])
-        RAVEN_CONFIG = locs_.setdefault('RAVEN_CONFIG', {})
+                _locals['INSTALLED_APPS'])
+        RAVEN_CONFIG = _locals.setdefault('RAVEN_CONFIG', {})
         RAVEN_CONFIG.setdefault('release', SENTRY_RELEASE)
         RAVEN_CONFIG['dsn'] = SENTRY_DSN
         RAVEN_CONFIG.setdefault(
@@ -363,7 +387,7 @@ def post_process_settings(globs=None):
             'raven.transport.requests.RequestsHTTPTransport')
         # If you are using git, you can also automatically
         # configure the release based on the git info.
-        LOGGING = locs_.setdefault('LOGGING', copy.deepcopy(DEFAULT_LOGGING))
+        LOGGING = _locals.setdefault('LOGGING', copy.deepcopy(DEFAULT_LOGGING))
         LOGGING['disable_existing_loggers'] = True
         LOGGING.setdefault('handlers', {}).update({
             'sentry': {
@@ -373,41 +397,43 @@ def post_process_settings(globs=None):
         root = LOGGING.setdefault('root', {})
         root['handlers'] = ['sentry']
         if SENTRY_TAGS and isinstance(SENTRY_TAGS, six.string_types):
-            locs_['SENTRY_TAGS'] = {}
-            for a in SENTRY_TAGS.split(','):
-                tag, val = a.split(':')[0], ':'.join(a.split(':')[1:])
-                if not val:
-                    val, tag = tag, 'general'
-                locs_['SENTRY_TAGS'][tag] = val
-        if 'DEPLOY_ENV' in locs_:
-            locs_['RAVEN_CONFIG']['environment'] = locs_['DEPLOY_ENV']
+            _locals['SENTRY_TAGS'] = {}
+            for tagentry in SENTRY_TAGS.split(','):
+                tag = tagentry.split(':')[0]
+                value = ':'.join(a.split(':')[1:])
+                if not value:
+                    value = tag
+                    tag = 'general'
+                _locals['SENTRY_TAGS'][tag] = value
+        if 'DEPLOY_ENV' in _locals:
+            _locals['RAVEN_CONFIG']['environment'] = _locals['DEPLOY_ENV']
     {%- endif %}
-    globals().update(locs_)
-    return locs_, filter_globals(globals()), env
+    globals().update(_locals)
+    return _locals, filter_globals(globals()), env
 
 
 def set_prod_settings(globs):
     '''
     Additional post processing of settings only ran on hosted environments
     '''
-    locs_, env = locals_settings_update(locals(), globs)
-    SERVER_EMAIL = locs_.setdefault(
+    _locals, env = locals_settings_update(locals(), globs)
+    SERVER_EMAIL = _locals.setdefault(
         'SERVER_EMAIL',
         '{env}-{{cookiecutter.lname}}@{{cookiecutter.tld_domain}}'.format(env=env))
-    locs_.setdefault('ADMINS', [('root', SERVER_EMAIL)])
-    locs_.setdefault('EMAIL_HOST', 'localhost')
-    locs_.setdefault('DEFAULT_FROM_EMAIL', SERVER_EMAIL)
-    ALLOWED_HOSTS = locs_.setdefault('ALLOWED_HOSTS', [])
-    CORS_ORIGIN_WHITELIST = locs_.setdefault(
+    _locals.setdefault('ADMINS', [('root', SERVER_EMAIL)])
+    _locals.setdefault('EMAIL_HOST', 'localhost')
+    _locals.setdefault('DEFAULT_FROM_EMAIL', SERVER_EMAIL)
+    ALLOWED_HOSTS = _locals.setdefault('ALLOWED_HOSTS', [])
+    CORS_ORIGIN_WHITELIST = _locals.setdefault(
         'CORS_ORIGIN_WHITELIST', tuple())
     # those settings by default are empty, we need to handle this case
     if not CORS_ORIGIN_WHITELIST:
-        locs_['CORS_ORIGIN_WHITELIST'] = (
+        _locals['CORS_ORIGIN_WHITELIST'] = (
             '{env}-{{cookiecutter.lname}}.{{cookiecutter.tld_domain}}'.format(env=env),  #noqa
             '.{{cookiecutter.tld_domain}}')
     if not ALLOWED_HOSTS:
-        locs_['ALLOWED_HOSTS'] = [
+        _locals['ALLOWED_HOSTS'] = [
             '{env}-{{cookiecutter.lname}}.{{cookiecutter.tld_domain}}'.format(env=env),  # noqa
             '.{{cookiecutter.tld_domain}}']
-    globals().update(locs_)
-    return locs_, filter_globals(globals()), env
+    globals().update(_locals)
+    return _locals, filter_globals(globals()), env
