e74840162437dfdc5ed852cf4c976f5156663bb8
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-10-11 22:47:46 +0200

    non existant settings

--- {{cookiecutter.out_dir}}/src/{{cookiecutter.django_project_name}}/settings/prod.py
+++ {{cookiecutter.out_dir}}/src/{{cookiecutter.django_project_name}}/settings/prod.py
@@ -15,7 +15,6 @@ SESSION_COOKIE_SECURE = True
 ACCOUNT_DEFAULT_HTTP_PROTOCOL = '{{cookiecutter.settings_account_default_http_protocol}}'
 SECURE_SSL_REDIRECT = {% if cookiecutter.settings_secure_ssl_redirect%}True{%else%}False{%endif%}
 
-MEDIA_ACCEL_REDIRECT = False
 SESSION_ENGINE = "{{cookiecutter.session_engine_prod}}"
 
 locs_, globs_, env = post_process_settings(locals())
