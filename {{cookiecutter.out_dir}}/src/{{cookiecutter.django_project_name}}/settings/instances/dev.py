8ebce2cf6d181685bfe5e62e9e49a89c477ea777
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2020-03-03 10:53:23 +0100

    fix settings

--- {{cookiecutter.out_dir}}/src/{{cookiecutter.django_project_name}}/settings/instances/dev.py
+++ {{cookiecutter.out_dir}}/src/{{cookiecutter.django_project_name}}/settings/instances/dev.py
@@ -7,7 +7,7 @@ from ..prod import *
 locs_, globs_, env = set_prod_settings(locals())
 globals().update(globs_)
 # Already set in ansible code, only here for example
-# hosts = ['0.0.0.0', 'staging.mixity.co', '.mixity.co',]
+# hosts = ['0.0.0.0', 'staging.foo.bar', '.foo.bar',]
 # CORS_ORIGIN_WHITELIST = CORS_ORIGIN_WHITELIST + tuple(hosts)
 # ALLOWED_HOSTS.extend(hosts)
 # vim:set et sts=4 ts=4 tw=80:
