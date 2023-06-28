03a8a3a96a78378a773662c71103e476eb178fad
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2023-06-28 21:04:27 +0100

    Factorize COMPOSE_FILE/CONTROL_COMPOSE_FILES

--- {{cookiecutter.out_dir}}/control.sh
+++ {{cookiecutter.out_dir}}/control.sh
@@ -28,35 +28,7 @@ if [[ -n $SHELL_DEBUG ]];then set -x;fi
 
 shopt -s extglob
 
-VENV=../venv
-APP={{cookiecutter.app_type}}
-APP_USER=${APP_USER:-${APP}}
-APP_CONTAINER=${APP_CONTAINER:-${APP}}
-APP_CONTAINERS="^($APP_CONTAINER|celery)"
-DEBUG=${DEBUG-}
-NO_BACKGROUND=${NO_BACKGROUND-}
-BUILD_PARALLEL=${BUILD_PARALLEL-1}
-BUILD_CONTAINERS="$APP_CONTAINER{%-if not cookiecutter.remove_cron%} cron{%endif%}"
-EDITOR=${EDITOR:-vim}
-DIST_FILES_FOLDERS=". src/*/settings"
-CONTROL_COMPOSE_FILES="${CONTROL_COMPOSE_FILES:-docker-compose.yml docker-compose-dev.yml}"
-COMPOSE_COMMAND=${COMPOSE_COMMAND:-docker-compose}
 ENV_FILES="${ENV_FILES:-.env docker.env}"
-USE_TOX_DIRECT=${USE_TOX_DIRECT-1}
-NO_SITE_PACKAGES=${NO_SITE_PACKAGES-1}
-TOX_ARGS="${TOX_ARGS-}"
-if [[ -n $NO_SITE_PACKAGES ]];then
-    TOX_ARGS="$TOX_ARGS --sitepackages"
-fi
-NO_DEVELOP=${NO_DEVELOP-}
-# special case: be sure to define some docker internal variables but let them overridable through .env
-export DOCKER_BUILDKIT="${DOCKER_BUILDKIT-1}"
-export COMPOSE_DOCKER_CLI_BUILD="${COMPOSE_DOCKER_CLI_BUILD-1}"
-export BUILDKIT_PROGRESS="${BUILDKIT_PROGRESS-plain}"
-export BUILDKIT_INLINE_CACHE="${BUILDKIT_INLINE_CACHE-1}"
-
-join_by() { local IFS="$1"; shift; echo "$*"; }
-
 source_envs() {
     set -o allexport
     for i in $ENV_FILES;do
@@ -75,17 +47,54 @@ source_envs() {
     done
     set +o allexport
 }
+source_envs
+
+VENV=../venv
+APP={{cookiecutter.app_type}}
+APP_USER=${APP_USER:-${APP}}
+APP_CONTAINER=${APP_CONTAINER:-${APP}}
+APP_CONTAINERS="^($APP_CONTAINER|celery)"
+DEBUG=${DEBUG-}
+NO_BACKGROUND=${NO_BACKGROUND-}
+BUILD_PARALLEL=${BUILD_PARALLEL-1}
+BUILD_CONTAINERS="$APP_CONTAINER{%-if not cookiecutter.remove_cron%} cron{%endif%}"
+EDITOR=${EDITOR:-vim}
+DIST_FILES_FOLDERS=". src/*/settings"
+# support both former $CONTROL_COMPOSE_FILES & $COMPOSE_FILE
+DEFAULT_CONTROL_COMPOSE_FILES="${DEFAULT_CONTROL_COMPOSE_FILES-docker-compose.yml docker-compose-dev.yml}"
+if [[ -n "$FORCE_OSX_SYNC" ]]; then
+    DEFAULT_CONTROL_COMPOSE_FILES="$DEFAULT_CONTROL_COMPOSE_FILES docker-compose-darwin.yml"
+fi
+CONTROL_COMPOSE_FILES="${CONTROL_COMPOSE_FILES:-$DEFAULT_CONTROL_COMPOSE_FILES}"
+COMPOSE_COMMAND=${COMPOSE_COMMAND:-docker-compose}
+USE_TOX_DIRECT=${USE_TOX_DIRECT-1}
+NO_SITE_PACKAGES=${NO_SITE_PACKAGES-1}
+TOX_ARGS="${TOX_ARGS-}"
+if [[ -n $NO_SITE_PACKAGES ]];then
+    TOX_ARGS="$TOX_ARGS --sitepackages"
+fi
+NO_DEVELOP=${NO_DEVELOP-}
+# special case: be sure to define some docker internal variables but let them overridable through .env
+export DOCKER_BUILDKIT="${DOCKER_BUILDKIT-1}"
+export COMPOSE_DOCKER_CLI_BUILD="${COMPOSE_DOCKER_CLI_BUILD-1}"
+export BUILDKIT_PROGRESS="${BUILDKIT_PROGRESS-plain}"
+export BUILDKIT_INLINE_CACHE="${BUILDKIT_INLINE_CACHE-1}"
+
+join_by() { local IFS="$1"; shift; echo "$*"; }
 
 set_dc() {
-    local COMPOSE_FILES="${@:-${CONTROL_COMPOSE_FILES}}"
-    DC="${COMPOSE_COMMAND}"
-    for i in $COMPOSE_FILES;do
-        DC="${DC} -f $i"
-    done
-    DCB="$DC -f docker-compose-build.yml"
-    if (echo $DC|grep -q  -- -dev );then
-        DCB="$DCB -f docker-compose-build-dev.yml"
+    export COMPOSE_FILE_RUN="${@:-${COMPOSE_FILE-${CONTROL_COMPOSE_FILES// /:}}}"
+    export COMPOSE_FILE="${COMPOSE_FILE_RUN}"
+    DC="${COMPOSE_COMMAND}";DCB="${DC}"
+    if [[ -z $COMPOSE_FILE ]];then
+        export COMPOSE_FILE_BUILD="docker-compose-build.yml"
+    else
+        export COMPOSE_FILE_BUILD="$COMPOSE_FILE:docker-compose-build.yml"
     fi
+    if (echo "$COMPOSE_FILE"|grep -q  -- -dev );then
+        export COMPOSE_FILE_BUILD="$COMPOSE_FILE_BUILD:docker-compose-build-dev.yml"
+    fi
+    export CONTROL_COMPOSE_FILES="${COMPOSE_FILE//:/ }"
 }
 
 log(){ echo "$@">&2;}
@@ -130,7 +139,7 @@ _shell() {
             -e SHELL_USER=${SHELL_USER} \
             $container $bargs
     else
-        set -- dvv $DC \
+        set -- dvv do_dcompose \
             $run_mode $run_mode_args \
             -e TERM=$TERM -e COLUMNS=${COLUMNS:-80} -e LINES=${LINES:-40} \
             -e SHELL_USER=${SHELL_USER} \
@@ -142,13 +151,13 @@ _shell() {
 #  dcompose $@: wrapper to docker-compose
 do_dcompose() {
     set -- dvv $DC "$@"
-    "$@"
+    ( export COMPOSE_FILE="$COMPOSE_FILE_RUN" && "$@" )
 }
 
 #  dbcompose $@: wrapper to docker-compose with build compose files set
 do_dbcompose() {
-    set -- dvv $DCB "$@"
-    "$@"
+    set -- dvv do_dbcompose "$@"
+    ( export COMPOSE_FILE="$COMPOSE_FILE_BUILD" && "$@" )
 }
 
 
@@ -216,21 +225,21 @@ do_install_docker() {
 
 #  pull [$args]: pull stack container images
 do_pull() {
-    vv $DC pull $@
+    vv do_dcompose pull $@
 }
 
 
 #  ps [$args]: ps
 do_ps() {
     local bargs=$@
-    set -- vv $DC ps
+    set -- vv do_dcompose ps
     $@ $bargs
 }
 
 #  up [$args]: start stack
 do_up() {
     local bargs=$@
-    set -- vv $DC up
+    set -- vv do_dcompose up
     if [[ -z $NO_BACKGROUND ]];then bargs="-d $bargs";fi
     $@ $bargs
 }
@@ -239,40 +248,40 @@ do_up() {
 #  run [$args]: run stack
 do_run() {
     local bargs=$@
-    set -- vv $DC run
+    set -- vv do_dcompose run
     $@ $bargs
 }
 
 #  rm [$args]: rm stack
 do_rm() {
     local bargs=$@
-    set -- vv $DC rm
+    set -- vv do_dcompose rm
     $@ $bargs
 }
 
 #  down [$args]: down stack
 do_down() {
     local bargs=$@
-    set -- vv $DC down
+    set -- vv do_dcompose down
     $@ $bargs
 }
 
 #  stop [$args]: stop
 do_stop() {
     local bargs=$@
-    set -- vv $DC stop
+    set -- vv do_dcompose stop
     $@ $bargs
 }
 
 #  stop_containers [$args]: stop containers (app_container by default)
 stop_containers() {
-    for i in ${@:-$APP_CONTAINER};do $DC stop $i;done
+    for i in ${@:-$APP_CONTAINER};do do_dcompose stop $i;done
 }
 
 #  fg: launch app container in foreground (using entrypoint)
 do_fg() {
     stop_containers
-    vv $DC run --rm --no-deps --use-aliases --service-ports -e IMAGE_MODE=fg $APP_CONTAINER $@
+    vv do_dcompose run --rm --no-deps --use-aliases --service-ports -e IMAGE_MODE=fg $APP_CONTAINER $@
 }
 
 #  build [$args]: rebuild app containers ($BUILD_CONTAINERS)
@@ -284,7 +293,7 @@ do_build() {
     if [[ -n $BUILDKIT_INLINE_CACHE ]];then
         bp="${bp} --build-arg BUILDKIT_INLINE_CACHE=\"${BUILDKIT_INLINE_CACHE}\""
     fi
-    set -- vv $DCB build $bp
+    set -- vv do_dbcompose build $bp
     if [[ -z "$bargs" ]];then
         for i in $BUILD_CONTAINERS;do
             $@ $i
@@ -420,7 +429,7 @@ do_open_perms_valve() {
     OPENVALVE_INTERNAL_UID="${OPENVALVE_INTERNAL_UID-1000}"
     DEFAULT_OPENVALVE_ACL="u:$SUPEREDITOR:rwx,u:$OPENVALVE_INTERNAL_UID:rwx,d:u:$SUPEREDITOR:rwx,d:u:$OPENVALVE_INTERNAL_UID:rwx"
     OPENVALVE_ACL="${OPENVALVE_ACL:-$DEFAULT_OPENVALVE_ACL}"
-    dvv $DC run --no-deps --rm \
+    dvv do_dcompose run --no-deps --rm \
         -v "$OPENVALVE_SOURCE:/openvalve" \
         -e "SUPEREDITOR=$SUPEREDITOR" \
         -e "OPENVALVE_ACL=$OPENVALVE_ACL" \
@@ -437,7 +446,7 @@ do_open_perms_valve() {
 
 #  get_container_code: refresh local/code with parts of the container files to help IDEs to do their completion job
 do_get_container_code() {
-    dvv $DC run --no-deps --rm \
+    dvv do_dcompose run --no-deps --rm \
         -v $W/local/code:/output \
         --entrypoint bash $APP_CONTAINER \
         -ec '\
@@ -476,9 +485,9 @@ do_vscode() {
 
 #  [NO_BUILD=] do_make_docs: daemon to sync local files inside docker containers (volumes to be exact)
 do_make_docs() {
-    if [[ -z ${NO_BUILD-} ]];then $DCB build docs;fi
+    if [[ -z ${NO_BUILD-} ]];then do_dbcompose build docs;fi
     # by default container entrypoint sync data to output dir
-    $DCB run --rm \
+    do_dbcompose run --rm \
         -e NO_BUILD=${NO_BUILD-} \
         -e NO_INIT=${NO_HTML-} \
         -e NO_CLEAN=${NO_CLEAN-} \
@@ -504,7 +513,6 @@ do_main() {
     actions="$actions|doc|make_docs"
     actions="@($actions|$actions_{{cookiecutter.app_type}})"
     action=${1-}
-    source_envs
     if [[ -n $@ ]];then shift;fi
     set_dc
     case $action in
